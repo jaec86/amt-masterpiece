8849f8f63a90baa8a2e8e578a2a65067d9269ee9
Javier Estupinan <jestupinan@zeerbyte.com>
2020-06-07 16:46:30 +0200

    added inner shadow to show there is more content available

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <transition enter-class="opacity-0">
       <router-view class="fixed inset-0 bg-gray-300 transition-all duration-500 delay-500" />
     </transition>
-    <div class="fixed top-0 w-full h-full bg-gray-800" :class="{ 'move-left-delay': $store.state.overlayPos === 'left', 'move-right-delay': $store.state.overlayPos === 'right' }" style="left: -100%"></div>
-    <div class="fixed top-0 w-full h-full bg-gray-500" :class="{ 'move-left': $store.state.overlayPos === 'left', 'move-right': $store.state.overlayPos === 'right' }" style="left: -100%"></div>
+    <div class="fixed top-0 w-full h-full bg-gray-900" :class="{ 'move-left-delay': $store.state.overlayPos === 'left', 'move-right-delay': $store.state.overlayPos === 'right' }" style="left: -100%"></div>
+    <div class="fixed top-0 w-full h-full bg-gray-700" :class="{ 'move-left': $store.state.overlayPos === 'left', 'move-right': $store.state.overlayPos === 'right' }" style="left: -100%"></div>
   </div>
 </template>
